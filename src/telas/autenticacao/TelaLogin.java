8bbbe870e82c61ff2020369d7bb1a295346cd758
gihnelli <giovannanelli71@gmail.com>
2026-05-27 11:07:38 -0300

    alterando imagem

--- src/telas/autenticacao/TelaLogin.java
+++ src/telas/autenticacao/TelaLogin.java
@@ -213,7 +213,7 @@ public class TelaLogin extends JFrame {
 
         public PainelFundo() {
             try {
-                imagemFundo = new ImageIcon("imagens/Menu.png").getImage();
+                imagemFundo = new ImageIcon("imagens/login.png").getImage();
             } catch (Exception e) {
                 System.err.println("Erro ao carregar imagem de fundo: " + e.getMessage());
             }
